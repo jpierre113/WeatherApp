6d1e12fda9605e93cf0ec9a74a7b373d7d22f406
Janna Pierre <gojanna@aol.com>
2019-10-31 16:21:13 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,107 @@
+console.log("main.js is connected!");
+document.getElementById("getWeather").addEventListener("click", getWeather);
+
+function getWeather(x) {
+  x.preventDefault;
+  fetch(
+    "http://api.openweathermap.org/data/2.5/weather?zip=" +
+      document.getElementById("zipcode").value +
+      "&units=imperial&appid=b90b1f863d30472ac7f514b69530df9e"
+  )
+    .then(res => {
+      return res.json();
+    })
+    .then(res => {
+      console.log(res);
+      getData(res);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+const getData = function(data) {
+  console.log(data);
+  const cityName = data.name;
+  const currentTemp = data.main.temp;
+  const descrip = data.weather[0].description;
+  const minTemp = data.main.temp_min;
+  const maxTemp = data.main.temp_max;
+  const longitude = data.coord.lon;
+  const latitude = data.coord.lat;
+  const humidity = data.main.humidity;
+  const windSpeed = data.wind.speed;
+
+  manipulateDom(
+    cityName,
+    currentTemp,
+    descrip,
+    minTemp,
+    maxTemp,
+    longitude,
+    latitude,
+    humidity,
+    windSpeed
+  );
+};
+//data from API will be inserted into variable
+const manipulateDom = function(
+  cityName,
+  currentTemp,
+  descrip,
+  minTemp,
+  maxTemp,
+  longitude,
+  latitude,
+  humidity,
+  windSpeed
+) {
+  document.getElementById("cityName").innerText = cityName;
+  let tempHTML = document.getElementById("currentTemp");
+  tempHTML.innerText = currentTemp + "°"; //varible used for color change
+  document.getElementById("descrip").innerText = descrip;
+  document.getElementById("minTemp").innerText = "Min\n" + minTemp + "°";
+  document.getElementById("maxTemp").innerText = "Max\n" + maxTemp + "°";
+  document.getElementById("longitude").innerText = "Longitude: " + longitude;
+  document.getElementById("latitude").innerText = "Latitude: " + latitude;
+  document.getElementById("humidity").innerText = "Humidity: " + humidity;
+  document.getElementById("windSpeed").innerText = "Wind Speed: " + windSpeed;
+  let searchBar = document.getElementById("searchBar");
+  searchBar.style.position = "absolute";
+  searchBar.style.top = "20px";
+  searchBar.style.right = "20px";
+  searchBar.style.left = "auto";
+
+  let container = document.querySelector(".container");
+  container.style.width = "50%";
+  container.style.backgroundColor = "lightBlue";
+  container.style.border = "2px solid";
+
+  let row = document.querySelector(".row");
+  row.style.border = "2px solid";
+
+  // temperature color will change
+  if (parseFloat(currentTemp) < 40) {
+    tempHTML.style.color = "blue";
+  } else if (parseFloat(currentTemp) > 90) {
+    tempHTML.style.color = "red";
+  } else tempHTML.style.color = "black";
+};
+
+/*
+
+Here's an overview of the steps you'll follow to get your app to work...
+
+STEPS
+
+1. when the page loads
+  - add an event listener to the button
+2. When the button is clicked
+  - grab the input
+  - store the value
+  - make an API request based on the input value
+3. When the API response is returned
+  - grab all the appropriate DOM elements
+  - append the data to the DOM
+
+*/
